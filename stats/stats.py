20b780fa426d79f8dc4463dcd1c0b239620b2190
Tuskle <lka.stopar@gmail.com>
2017-06-04 20:44:32 +0200

    Update stats.py

--- stats/stats.py
+++ stats/stats.py
@@ -1,11 +1,10 @@
 
 # coding: utf-8
 
-# In[1]:
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from rtree import index
 
 def read(location):
     return pd.read_csv(location)
@@ -32,61 +31,42 @@ df = pd.merge(agency, routes, on='agency_id')
 df = pd.merge(df, trips, on='route_id')
 df = pd.merge(df, stopTimes, on='trip_id')
 df = pd.merge(df, stops, on='stop_id')
-
-#print(df.groupby("agency_name").count().loc[:,['route_id']].sort_values(by='route_id'))
-
-#print(df.groupby("stop_name").count().loc[:,['route_id']].sort_values(by='route_id'))
-
 df = df.sort_values(by=['route_id','service_id', 'trip_id', 'direction_id', 'stop_sequence'], ascending=[True, True, True, True, True])
 
 
-# In[13]:
+#drop unnecesary cols
+df.drop(['shape_id','arrival_time', 'departure_time','pickup_type','drop_off_type','agency_id','agency_name','dobicek','promet','route_id','route_type','service_id','trip_id','direction_id','stop_id','stop_sequence'],axis=1,inplace=True)
 
-#df.drop(['shape_id'],axis=1,inplace=True)
-#'arrival_time', 'departure_time','pickup_type','drop_off_type''agency_id','agency_name','dobicek','promet''route_id','route_type','service_id','trip_id','direction_id','stop_id','stop_sequence'
-
+#remove repeated stops(example: same stop different direction)
 df=df.drop_duplicates()
-df
 
-
-# In[55]:
-
-from rtree import index
+#create an index for nearestneighbour
 idx = index.Index()
-left, bottom, right, top = (45, 13, 47, 15)
-#idx.insert(0,(left,bottom,right,top))
 
 tar = read('./data/google_feed/SI.csv')
+#fill index with city coordinates
 for i in tar.index:
     idx.insert(i,(tar.X[i],tar.Y[i],tar.X[i],tar.Y[i]))
-tar
-
-
-# In[56]:
-
+#for each stop find the nearest city with NN,in case of multiple with the same distance,pick first
 for i in range(df.shape[0]):
     left=df.iloc[i].stop_lat
     right=left
     bottom=df.iloc[i].stop_lon
     top=bottom
     tar.loc[list(idx.nearest((left,bottom,right,top),1))[0]:list(idx.nearest((left,bottom,right,top),1))[0],"SUM"]+=1
-tar
 
-
-# In[81]:
-
+#modified SI.csv with stop numbers and sorted
 yay = read('./data/google_feed/yay.csv')
 sums=yay["SUM"].tolist()
 nmv=yay["NMV"].tolist()
 pops=yay["POP"].tolist()
+
+
 plt.plot(nmv,sums)
 plt.xlabel("Nadmorska višina")
 plt.ylabel("Število povezav")
 plt.savefig('foo.png', bbox_inches='tight')
 plt.show()
-
-
-# In[76]:
 
 plt.plot(pops,sums,"ro")
 plt.xlabel("Število prebivalcev")
@@ -94,20 +74,6 @@ plt.ylabel("Število povezav")
 plt.savefig('prebs.png', bbox_inches='tight')
 plt.show()
 
-
-# In[77]:
-
-dif=[]
-for i in range(len(sums)):
-    dif.append(((sums[i]/47609)*100))
-plt.plot(nmv,dif)
-plt.xlabel("Nadmorska višina")
-plt.ylabel("Število povezav v %")
-plt.savefig('hei.png', bbox_inches='tight')
-plt.show()
-
-
-# In[80]:
 
 sumy=2064000
 test=[]
@@ -118,9 +84,5 @@ plt.xlabel("Nadmorska višina")
 plt.ylabel("Število povezav v %/število prebivalcev v %")
 plt.savefig('hei.png', bbox_inches='tight')
 plt.show()    
-
-
-# In[ ]:
-
 
 
